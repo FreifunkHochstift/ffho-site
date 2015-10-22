40ad05b0f86d4dd1b1858deeaf0a7f17517f5995
Karsten Böddeker <freifunk@kb-light.de>
2015-10-22 22:55:45 +0200

    Initial Commit

---
+++ site.mk
@@ -0,0 +1,38 @@
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-14 \
+	ffpb-banner \
+	ffpb-debug \
+    ffpb-node-tuning \
+	ffpb-ebtables-net-rules \
+	ffpb-openwrt-migration-tools \
+	gluon-alfred \
+	gluon-announced \
+	gluon-autoupdater \
+	gluon-config-mode-core \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-config-mode-geo-location \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	iwinfo \
+	iptables \
+	haveged
+
+
+# 0.2~1 < 0.2 < 0.2+1 < 0.2-1 < 0.2.1 < 0.3
+DEFAULT_GLUON_RELEASE := 0.6.5~exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_PRIORITY := 0
+
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+GLUON_PRIORITY ?= $(DEFAULT_GLUON_PRIORITY)
+GLUON_LANGS ?= en de
