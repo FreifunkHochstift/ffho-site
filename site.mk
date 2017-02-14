84eadce52b440aba04e2caa91d790ffd0c84f74b
Karsten Böddeker <freifunk@kb-light.de>
2017-02-14 15:45:28 +0100

    update config for site-split

--- site.mk
+++ site.mk
@@ -1,6 +1,5 @@
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-14 \
-	gluon-alfred \
+	gluon-mesh-batman-adv-15 \
 	ffho-ap-timer \
 	gluon-respondd \
 	ffho-ath9k-blackout-workaround \
@@ -29,7 +28,6 @@ GLUON_SITE_PACKAGES := \
 	gluon-mesh-vpn-fastd \
 	gluon-next-node \
 	gluon-radvd \
-	ffho-respondd-config \
 	gluon-setup-mode \
 	ffho-site-auto-select \
 	ffho-status-page \
@@ -64,7 +62,7 @@ ifeq ($(GLUON_TARGET),x86-64)
 endif
 
 # 0.2~1 < 0.2 < 0.2+1 < 0.2-1 < 0.2.1 < 0.3
-DEFAULT_GLUON_RELEASE := 0.7.5~exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 1.0.0~exp$(shell date '+%Y%m%d')
 DEFAULT_GLUON_PRIORITY := 0
 
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
