6c8ac6d0894b1e65a6f7bfb2df143d67f0fb28da
Michael Schwarz <post@michael-schwarz.name>
2018-12-11 11:31:12 +0100

    Update modules and site.mk (use features instead of specific packages)

--- site.mk
+++ site.mk
@@ -1,31 +1,25 @@
+GLUON_FEATURES := \
+        autoupdater \
+        ebtables-filter-multicast \
+        ebtables-filter-ra-dhcp \
+        ebtables-limit-arp \
+        mesh-batman-adv-15 \
+        mesh-vpn-fastd \
+        radvd \
+        respondd \
+        status-page \
+        web-advanced \
+        web-wizard
+
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-15 \
-	gluon-respondd \
+	-gluon-web-autoupdater \
 	ffho-ap-timer \
-	gluon-autoupdater \
 	ffho-autoupdater-wifi-fallback \
 	ffho-banner \
-	gluon-config-mode-core \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-contact-info \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
 	ffho-config-mode-site-select \
 	ffho-debug \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-ebtables-source-filter \
-	gluon-mesh-vpn-fastd \
-	gluon-radvd \
-	gluon-setup-mode \
-	gluon-status-page \
-	gluon-web-admin \
 	ffho-web-ap-timer \
 	ffho-web-autoupdater \
-	gluon-web-logging \
-	gluon-web-network \
-	gluon-web-wifi-config \
 	iwinfo \
 	haveged
 
@@ -44,7 +38,6 @@ USB_NIC := \
 WLAN := \
 	kmod-ath9k \
 	kmod-ath10k-ct \
-	ath10k-firmware-qca988x-ct
 
 ifeq ($(GLUON_TARGET),x86-generic)
 	GLUON_SITE_PACKAGES += \
